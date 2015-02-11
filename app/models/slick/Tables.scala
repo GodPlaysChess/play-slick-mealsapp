2dd675d65ccb4860a331efa4968f9b2d4e6019c3
GodPlaysChess <pumabox@gmail.com>
2015-02-11 16:32:49 +0100

    changed non-unqiue column name to unque

--- app/models/slick/Tables.scala
+++ app/models/slick/Tables.scala
@@ -37,11 +37,11 @@ object Tables {
   }
   
   class Users(tag: Tag) extends Table[User](tag, "USERS") {
-    def id = column[Long]("USER_ID", O.PrimaryKey, O.AutoInc)
+    def userid = column[Long]("USER_ID", O.PrimaryKey, O.AutoInc)
     def name = column[String]("USERNAME")
     def password = column[String]("PASSWORD")
     
-    override def * : ProvenShape[User] = (id, name, password) <> (User.tupled, User.unapply)
+    override def * : ProvenShape[User] = (userid, name, password) <> (User.tupled, User.unapply)
   }
   
 
